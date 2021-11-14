197bafa6a84cc19d5195ab2f7e882dc1a365fb13
noureddein <noureddein@gmail.com>
2021-11-14 23:50:13 +0200

    adding animations

--- public/app.js
+++ public/app.js
@@ -166,3 +166,42 @@ let typed_2 = new Typed(".typing-2", {
     backSpeed: 60,
     loop: true
 })
+
+
+
+var scroll = window.requestAnimationFrame ||
+    function (callback) { window.setTimeout(callback, 1000 / 60) };
+
+var elementsToShow = document.querySelectorAll('.show-on-scroll');
+console.log(elementsToShow)
+
+function loop() {
+
+    elementsToShow.forEach(function (element) {
+        if (isElementInViewport(element)) {
+            element.classList.add('is-visible');
+        } else {
+            element.classList.remove('is-visible');
+        }
+    });
+
+    scroll(loop);
+}
+loop();
+
+function isElementInViewport(el) {
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+    var rect = el.getBoundingClientRect();
+    return (
+        (rect.top <= 0
+            && rect.bottom >= 0)
+        ||
+        (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+        ||
+        (rect.top >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    );
+}
